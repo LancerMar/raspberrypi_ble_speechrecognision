053e9046c76d724943b8f1d046e921858006b1da
LancerMar <602545140@qq.com>
2022-08-06 01:38:22 +0100

    some change

--- Audio_sys/textToSpeeh.py
+++ Audio_sys/textToSpeeh.py
@@ -4,7 +4,7 @@ import pyttsx3
 def speek(text_speek):
     engine = pyttsx3.init()
     engine.setProperty('rate', 125)
-    # engine.say("turn left and go straight to the end of this road")
+    # engie.say("turn left and go straight to the end of this road")
     engine.say(text_speek)
     engine.runAndWait()
 
